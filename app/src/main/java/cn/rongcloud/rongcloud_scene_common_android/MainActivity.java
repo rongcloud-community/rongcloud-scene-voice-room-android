301189c18ed8b4bc2d08a0787ecb7b8030445291
guoyaning <guoyaning@rongcloud.cn>
2022-04-22 17:02:27 +0800

    build: fix build project

--- app/src/main/java/cn/rongcloud/rongcloud_scene_common_android/MainActivity.java
+++ app/src/main/java/cn/rongcloud/rongcloud_scene_common_android/MainActivity.java
@@ -1,14 +1,39 @@
 package cn.rongcloud.rongcloud_scene_common_android;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.basis.ui.PermissionActivity;
+import com.basis.utils.KToast;
+import com.basis.wapper.IResultBack;
 
-import android.os.Bundle;
+import cn.rongcloud.config.router.RouterPath;
 
-public class MainActivity extends AppCompatActivity {
+@Route(path = RouterPath.ROUTER_MAIN)
+public class MainActivity extends PermissionActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int setLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected String[] onCheckPermission() {
+        return LAUNCHER_PERMISSIONS;
+    }
+
+    @Override
+    protected void onAccept(boolean accept) {
+        findViewById(R.id.iv_voice_room).setOnClickListener(v -> {
+            checkAndRequestPermissions(VOICE_PERMISSIONS, new IResultBack<Boolean>() {
+                @Override
+                public void onResult(Boolean aBoolean) {
+                    if (aBoolean) {
+                        ARouter.getInstance().build(RouterPath.ROUTER_VOICE_LIST).navigation();
+                    } else {
+                        KToast.show("请赋予必要的权限");
+                    }
+                }
+            });
+        });
     }
 }
